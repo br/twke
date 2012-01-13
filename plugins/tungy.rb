f68355192b1577ea03ff6537d6651d40c8f12be5
winton <mail@wintoni.us>
2012-01-12 16:28:13 -0800

    Making Tungy plugin case insensitive

--- plugins/tungy.rb
+++ plugins/tungy.rb
@@ -3,7 +3,7 @@ require 'twss'
 class Plugin::Tungy < Plugin
 
   def add_routes(rp, opts)
-    rp.route /tungy/ do |act|
+    rp.route /tungy/i do |act|
       act.say "http://memegenerator.net/cache/instances/400x/12/12859/13167654.jpg"
     end
   end
