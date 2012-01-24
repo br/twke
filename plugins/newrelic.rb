1fa9b36b6ed291cd6b9035f60e35a4a6a2983637
Michael Schaefermeyer <michael.schaefermeyer@zweitag.de>
2012-01-24 15:52:35 +0100

    Refactored new relic

--- plugins/newrelic.rb
+++ plugins/newrelic.rb
@@ -10,26 +10,8 @@ class Plugin::Newrelic < Plugin
       act.say "Here is how we do: #{metrics['Response Time']} at #{metrics['Throughput']} with an error rate of #{metrics['Error Rate']}. That gives us an Apdex score of #{metrics['Apdex']}."
     end
 
-    rp.route 'start error watcher' do |act|
-      act.say "Starting error watcher"
-      @error_watcher && Thread.kill(@error_watcher)
-      @error_watcher = Thread.new do
-        told_em_at = 0
-        loop do
-          if told_em_at < Time.now.to_i - 1800
-            error_rate = get_newrelic["Error Rate"]
-            if error_rate.to_f > 0.05
-              act.say "Sorry to interrupt but our error rate is at #{error_rate}. Do something!"
-              told_em_at = Time.now.to_i
-            end
-          end
-          sleep 300
-        end
-      end
-    end
-
-    rp.route 'check error watcher' do |act|
-      act.say "Error watcher is #{@error_watcher.status}"
+    rp.route /(?<action>start|stop|check) error watcher/ do |act|
+      call("newrelic_#{act.action}", act)
     end
 
     rp.route 'stop error watcher' do |act|
@@ -41,11 +23,12 @@ class Plugin::Newrelic < Plugin
   private
 
   def get_newrelic
-    account_id                   = 59
-    application_id               = 136960
+    account_id                   = Twke::Conf.get('newrelic.account')
+    application_id               = Twke::Conf.get('newrelic.application')
+    api_key                      = Twke::Conf.get('newrelic.api-key')
+
     request                      = HTTPI::Request.new "https://api.newrelic.com/accounts/#{account_id}/applications/#{application_id}/threshold_values.xml"
-    request.headers["x-api-key"] = "b2751da5c301d17265b23fba34da4cd7829b9120b46e96d"
-    request.auth.ssl.verify_mode = :none
+    request.headers["x-api-key"] = api_key
     html                         = HTTPI.get(request).body
     xml                          = Nokogiri::XML.parse html
 
@@ -54,6 +37,33 @@ class Plugin::Newrelic < Plugin
       metrics[measure] = xml.at_css("threshold_value[name='#{measure}']")["formatted_metric_value"]
     end
     metrics
+  end
+
+  def newrelic_check act
+    act.say "Error watcher is #{@error_watcher && @error_watcher.status}"
+  end
+
+  def newrelic_start act
+    act.say "Starting error watcher"
+    @error_watcher && Thread.kill(@error_watcher)
+    @error_watcher = Thread.new do
+      told_em_at = 0
+      loop do
+        if told_em_at < Time.now.to_i - 1800
+          error_rate = get_newrelic["Error Rate"]
+          if error_rate.to_f > Twke::Conf.get('newrelic.threshold')
+            act.say "Sorry to interrupt but our error rate is at #{error_rate}. Do something!"
+            told_em_at = Time.now.to_i
+          end
+        end
+        sleep 300
+      end
+    end
+  end
+
+  def newrelic_stop act
+    @error_watcher && Thread.kill(@error_watcher)
+    act.say "Killed it!"
   end
 
 end
