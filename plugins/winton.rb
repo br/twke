9b53f1bbea39e7cf394280cbe069e3f775ece933
winton <mail@wintoni.us>
2012-02-14 13:20:03 -0800

    Revert "Winton plugin"
    This reverts commit eec5b418ac39f28bf49ec3b49ed9624d4e857700.

--- plugins/winton.rb
+++
@@ -1,8 +0,0 @@
-class Plugin::Winton < Plugin
-
-  def add_routes(rp, opts)
-    rp.route /(.*winton.*code.*)|(.*code.*winton.*)/i, :noprefix => true do |act|
-      act.say "http://f.cl.ly/items/1J2n0f2O4607083M3q00/winton%20f%20u.gif"
-    end
-  end
-end
