6a11874f559e01d1a5227aa2b8631d2dfd53cdf8
Michael Schaefermeyer <michael.schaefermeyer@zweitag.de>
2012-03-19 17:32:57 -0700

    Lighthouse support

--- plugins/lighthouse.rb
+++ plugins/lighthouse.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 class Plugin::Lighthouse < Plugin
 
   def add_routes(rp, opts)
-    rp.route /#(?<id>\d+)/ do |act|
+    rp.route(/#(?<id>\d+)/, noprefix => true) do |act|
       act.say "That would be the following Lighthouse URL: https://bleacherreport.lighthouseapp.com/projects/6296-bug-reporting/tickets/#{act.id}"
     end
   end
